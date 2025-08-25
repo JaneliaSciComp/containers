6f11d3503f32c7634746910a6010f9ef252ceb22
Cristian Goina <goinac@janelia.hhmi.org>
2025-08-25 13:10:53 -0400

    renamed package

--- cellpose/4.0.6-dask2025.5.1-py12/scripts/python/setup.py
+++ cellpose/4.0.6-dask2025.5.1-py12/scripts/python/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="distributed_cellpose",
+    name="cellpose_tools",
     version="4.0.6",
     packages=setuptools.find_packages(),
     include_package_data=True,
