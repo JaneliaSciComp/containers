268901057ca330b44288eaa4c5439f98c5aac179
Cristian Goina <goinac@janelia.hhmi.org>
2025-08-25 13:09:28 -0400

    setup some dependencies for distributed cellpose

--- cellpose/4.0.6-dask2025.5.1-py12/scripts/python/setup.py
+++ cellpose/4.0.6-dask2025.5.1-py12/scripts/python/setup.py
@@ -2,10 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="distributed_cellpose",
-    version="2.2.3",
+    version="4.0.6",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "numpy>=1.26.0,<2"
+        "cellpose==4.0.6",
+        "dask==2025.5.1",
+        "distributed==2025.5.1",
+        "numpy>=1.26.0,<2",
+        "zarr>=2.18,<3"
     ]
 )
